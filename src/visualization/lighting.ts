2a1d8d4ba68d1293d8cf20fcbc334bbdfa09311a
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-08-12 11:38:18 +0300

    light the scene with directional light(sun light)

---
+++ src/visualization/lighting.ts
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+
+export function createSunLight(): THREE.DirectionalLight {
+  const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+  sunLight.position.set(-2, 0.5, 1.5);
+  return sunLight;
+}
