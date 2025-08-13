340ef25cdae782d870573fd1f33a21c3d7ce7572
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-08-13 16:19:44 +0300

    update lighting intensity

--- src/visualization/lighting.ts
+++ src/visualization/lighting.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 export function createSunLight(): THREE.DirectionalLight {
-  const sunLight = new THREE.DirectionalLight(0xffffff, 4.0);
+  const sunLight = new THREE.DirectionalLight(0xffffff, 3.0);
   sunLight.position.set(-2, 0.5, 1.5);
   return sunLight;
 }
